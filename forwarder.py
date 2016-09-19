f4c2e5cbdfd4ef7f392671ea65ffc1573da7f104
Jim Zhong <zhonghc@staff.zjutv.com>
2016-09-19 22:38:05 +0800

    use redis for cache

--- forwarder.py
+++ forwarder.py
@@ -5,122 +5,27 @@
 import time
 import sys
 import logging
-import socket
-import signal
 import json
-from time import sleep
+import redis
+import pickle
+import socket
 from dnslib.server import DNSServer, BaseResolver, DNSLogger
 from dnslib.dns import DNSError, QTYPE, RCODE, RR, A
 from dnslib import DNSRecord
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
 class CachedReply(object):
     '''
     A cached DNS reply, for making replies from cache
     '''
-    def __init__(self, reply, ttl_lower_bound=60):
-        self.access_time = time.time()
-        for x in reply.rr:
-            x.ttl = max(x.ttl, ttl_lower_bound)
-        minttl = min([x.ttl for x in reply.rr])
-        self.expire_time = self.access_time + minttl
+    def __init__(self, reply):
         self.reply = reply
 
     def make_reply(self, request):
         header = request.reply().header
         self.reply.header = header
-        #Change reply header in order to match request
-        now = time.time()
-        for rr in self.reply.rr:
-            delta = int(now - self.access_time)
-            rr.ttl -= delta
-            #Modify TTL
-        self.access_time = now
         return self.reply
-
-    def is_valid(self):
-        return time.time() < self.expire_time
-
-
-class Node(object):
-    '''
-    One node in cache/double-linked list, contain key and value
-    '''
-    def __init__(self, key, value):
-        self.prev = self.next = None
-        self.key = key
-        self.value = value
-
-    def __str__(self):
-        return "<Node {}>".format(self.key)
-
-
-class DNSCache(object):
-
-    def __init__(self, capacity):
-        self.size = 0
-        self.capacity = capacity
-        self.dict = {}
-        self.head = Node('HEAD', None)
-        self.tail = Node('TAIL', None)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def __len__(self):
-        return self.size
-
-    def __contains__(self, key):
-        return key in self.dict
-
-    def __unlink_node(self, node):
-        self.size -= 1
-        logging.debug("Unlink {}".format(node))
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        node.prev = node.next = None
-        return node
-
-    def __remove_oldest(self):
-        node = self.tail.prev
-        logging.debug("Remove {} from cache".format(node))
-        assert node is not self.head
-        self.__unlink_node(node)
-        self.dict.pop(node.key)
-
-    def __insert_node_first(self, node):
-        self.size += 1
-        logging.debug("Inserting {} to first".format(node))
-        node.next = self.head.next
-        self.head.next.prev = node
-        node.prev = self.head
-        self.head.next = node
-
-    def __setitem__(self, key, value):
-        if key in self.dict:
-            node = self.__unlink_node(self.dict[key])
-            logging.debug("Updating {}".format(node))
-            node.value = value
-            self.__insert_node_first(node)
-        else:
-            item = Node(key, value)
-            self.dict[key] = item
-            self.__insert_node_first(item)
-            if self.size > self.capacity:
-                self.__remove_oldest()
-
-    def __getitem__(self, key):
-        #Client should handle KeyError exception
-        t = self.dict[key]
-        self.__unlink_node(t)
-        self.__insert_node_first(t)
-        return t.value
-
-    def __delitem__(self, key):
-        node = self.dict[key]
-        self.dict.pop(key)
-        self.__unlink_node(node)
-
 
 
 class MyResolver(BaseResolver):
@@ -128,7 +33,8 @@ class MyResolver(BaseResolver):
     def __init__(self, filename, cache_size=1000):
         super().__init__()
         self.load_config(filename)
-        self.cache = DNSCache(cache_size)
+        # self.cache = DNSCache(cache_size)
+        self.redis = redis.StrictRedis(host='localhost')
 
     @staticmethod
     def load_list_from_file(filename):
@@ -184,11 +90,14 @@ class MyResolver(BaseResolver):
 
     def resolve(self, request, handler):
         #Try to fetch from cache
-        key = (request.q.qname, request.q.qtype)
+        key = "{}:{}".format(request.q.qname, request.q.qtype)
+        logging.debug(key)
 
-        if key in self.cache and self.cache[key].is_valid():
-            logging.debug("Cache hit for {}".format(request.q.qname))
-            return self.cache[key].make_reply(request)
+        cached = self.redis.get(key)
+        if cached:
+            obj = pickle.loads(cached)
+            logging.debug("Cache hit for {}".format(key))
+            return obj.make_reply(request)
 
         #Do actual query
         try:
@@ -198,13 +107,13 @@ class MyResolver(BaseResolver):
                     logging.debug("{} matched in {} list".format(request.q.qname, name))
                     reply = self.resolve_from_upstream(request, name)
                     if reply.header.rcode == RCODE.NOERROR and reply.rr:
-                        self.cache[key] = CachedReply(reply, self.upstreams[name].get("ttl", 10))
+                        self.redis.set(key, pickle.dumps(CachedReply(reply)), ex=self.upstreams['default'].get("ttl", 10))
                     break
             else:
                 logging.debug("resolve {} from default server".format(request.q.qname))
                 reply = self.resolve_from_upstream(request, 'default')
                 if reply.header.rcode == RCODE.NOERROR and reply.rr:
-                    self.cache[key] = CachedReply(reply, self.upstreams['default'].get("ttl", 10))
+                    self.redis.set(key, pickle.dumps(CachedReply(reply)), ex=self.upstreams['default'].get("ttl", 10))
 
         except Exception as e:
             logging.error(e)
