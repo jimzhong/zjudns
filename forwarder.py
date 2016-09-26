8b5d620b78a7d564c1e4e10f0c77214a3bf2e18d
Jim Zhong <zhonghc@staff.zjutv.com>
2016-09-26 22:17:21 +0800

    use configparser

--- forwarder.py
+++ forwarder.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 
 import time
-import sys
 import logging
-import json
+import configparser
 import redis
 import pickle
 import socket
 import select
 import random
-# from dnslib.server import DNSServer, BaseResolver, DNSLogger
+import argparse
 from dnslib.dns import DNSError, QTYPE, RCODE, RR, A
 from dnslib import DNSRecord
-
-ALLOWED_QTYPE = (QTYPE.A, QTYPE.AAAA, QTYPE.MX, QTYPE.CNAME, QTYPE.NS, QTYPE.SRV)
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
@@ -23,8 +18,6 @@ class Server(object):
 
     def __init__(self, filename):
         self.load_config(filename)
-        self.load_hosts_file("hosts.txt")
-        self.redis = redis.StrictRedis(host='localhost')
 
     def load_hosts_file(self, filename):
         self.hosts = {}
@@ -34,21 +27,10 @@ class Server(object):
                     l = x.strip()
                     if l:
                         name, ip = l.split()
+                        logging.info("redirect {} to {}".format(name, ip))
                         self.hosts[tuple(reversed(name.split(".")))] = ip
         except IOError:
             logging.warning("Host file not found.")
-        logging.debug(self.hosts)
-
-    def load_from_hosts(self, request):
-        matched = self.domain_match_set(request.q.qname.label, self.hosts)
-        if matched:
-            # logging.debug("{} found in hosts".format(request.q.qname))
-            ip = self.hosts[matched]
-            reply = request.reply()
-            reply.add_answer(RR(request.q.qname, QTYPE.A, ttl=60, rdata=A(ip)))
-            return reply
-        else:
-            return None
 
     @staticmethod
     def load_list_from_file(filename):
@@ -66,14 +48,58 @@ class Server(object):
         return ret
 
     def load_config(self, filename):
-        with open(filename) as f:
-            self.upstreams = json.load(f)
-            logging.info(self.upstreams)
+        config = configparser.ConfigParser()
+        config.read(filename)
+
+        logging.getLogger().setLevel(getattr(logging, config['global']['log_level']))
+        self.load_hosts_file(config['global']['hosts_file'])
+        self.allowed_qtype = tuple(getattr(QTYPE, x) for x in map(str.strip, config['global']['allowed_qtype'].split(",")))
+        self.server_port = int(config['global'].get('port', 1053))
+        # print(self.allowed_qtype)
+
+        self.upstreams = {}
+        for x in config.sections():
+            if x != 'global':
+                self.upstreams[x] = {
+                    "file": config[x].get('file', '/dev/null'),
+                    "ttl": int(config[x]['ttl']),
+                    'timeout': int(config[x]['timeout'])
+                    }
+                servers = []
+                for addr in config[x]['servers'].split(','):
+                    ip = addr.split("#")[0].strip()
+                    port = int(addr.split("#")[1])
+                    servers.append((ip, port))
+                self.upstreams[x]['servers'] = tuple(servers)
+
         self.domains = {}
         for name, val in self.upstreams.items():
             if name != 'default':
                 self.domains[name] = self.load_list_from_file(val["file"])
                 logging.info("loaded {} domains from {}".format(len(self.domains[name]), val["file"]))
+
+    def load_from_hosts(self, request):
+        matched = self.domain_match_set(request.q.qname.label, self.hosts)
+        if matched:
+            # logging.debug("{} found in hosts".format(request.q.qname))
+            ip = self.hosts[matched]
+            reply = request.reply()
+            reply.add_answer(RR(request.q.qname, QTYPE.A, ttl=60, rdata=A(ip)))
+            return reply
+        else:
+            return None
+
+    def load_from_cache(self, request):
+        key = "dns:{}:{}".format(request.q.qname, request.q.qtype)
+        cached = self.redis.get(key)
+        if cached:
+            reply = pickle.loads(cached)
+            reply.header = request.reply().header
+            return reply
+        return None
+
+    def save_to_cache(self, key, reply):
+        self.redis.set(key, pickle.dumps(reply), ex=ttl)
 
     @staticmethod
     def domain_match_set(domain_tuple, target_set, depth=5):
@@ -108,17 +134,6 @@ class Server(object):
         except socket.error as e:
             logging.error(e)
 
-    def save_to_cache(self, key, reply):
-        self.redis.set(key, pickle.dumps(reply), ex=ttl)
-
-    def load_from_cache(self, key, request):
-        cached = self.redis.get(key)
-        if cached:
-            reply = pickle.loads(cached)
-            reply.header = request.reply().header
-            return reply
-        return None
-
     def send_reply_to(self, reply, addr):
         try:
             self.server_sock.sendto(reply.pack(), addr)
@@ -126,15 +141,15 @@ class Server(object):
             logging.error(e)
 
     def handle_client_request(self, data, addr):
-
         try:
             request = DNSRecord.parse(data)
         except Exception as e:
             logging.error(e)
+            return
 
         logging.info("request for {} from {}".format(request.q.qname, addr))
 
-        if request.q.qtype not in ALLOWED_QTYPE:
+        if request.q.qtype not in self.allowed_qtype:
             logging.info("not allowed qtype of {}".format(request.q.qtype))
             reply = request.reply()
             reply.header.rcode = RCODE.REFUSED
@@ -148,12 +163,9 @@ class Server(object):
                 self.send_reply_to(reply, addr)
                 return
 
-        #Try to fetch from cache
-        key = "dns:{}:{}".format(request.q.qname, request.q.qtype)
-
-        cached = self.load_from_cache(key, request)
+        cached = self.load_from_cache(request)
         if cached:
-            # TODO: Add TTL adjust
+            # TODO: Add TTL adjustment
             logging.info("cache hit on {}".format(request.q.qname))
             self.send_reply_to(cached, addr)
             return
@@ -169,7 +181,6 @@ class Server(object):
             else:
                 logging.debug("resolve {} from default server".format(request.q.qname))
                 self.send_to_upstream(request, 'default', addr)
-
         except Exception as e:
             logging.error(e)
             reply = request.reply()
@@ -191,21 +202,40 @@ class Server(object):
         for x in tmplist:
             self.waiting.pop(x)
 
+    def handle_server_reply(self, data, addr):
+        try:
+            reply = DNSRecord.parse(data)
+        except Exception as e:
+            logging.error(e)
+            return
 
-    def serve_forever(self, pool_size=4):
+        logging.info("reply for {} from {}".format(reply.q.qname, addr))
+
+        if (reply.header.id, addr) in self.waiting:
+            info = self.waiting.pop((reply.header.id, addr))
+            reply.header.id = info[4]
+            self.send_reply_to(reply, info[1])
+            if reply.header.rcode in (RCODE.NOERROR, RCODE.NXDOMAIN):
+                key = "dns:{}:{}".format(info[0].q.qname, info[0].q.qtype)
+                logging.debug("add {} to cache, ttl={}".format(key, info[3]))
+                self.redis.set(key, pickle.dumps(reply), ex=info[3])
+
+    def serve_forever(self, pool_size=10):
+        self.redis = redis.StrictRedis(host='localhost')
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server_sock.bind(("", 1053))
+        self.server_sock.bind(("", self.server_port))
         self.query_sock_pool = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(pool_size)]
         self.waiting = {}
         # key is (trans_id, server_addr), value is (request, client_addr, old_trans_id, timeout)
         self.trans_id = 0
-        # self.stat = {}
-        # for gathering statistics, key is server_addr(as a tuple), value is (timeout count, request count)
+        # TODO: use a random trans_id to avoid spoofing
 
         epoll = select.epoll()
         epoll.register(self.server_sock, select.EPOLLIN)
         for x in self.query_sock_pool:
             epoll.register(x, select.EPOLLIN)
+
+        logging.info("server started on port {}".format(self.server_port))
 
         while True:
             events = epoll.poll(timeout=1)
@@ -224,26 +254,9 @@ class Server(object):
             self.sweep_waiting_list()
 
 
-    def handle_server_reply(self, data, addr):
-        try:
-            reply = DNSRecord.parse(data)
-        except Exception as e:
-            logging.error(e)
-            return
-
-        logging.info("reply for {} from {}".format(reply.q.qname, addr))
-
-        if (reply.header.id, addr) in self.waiting:
-            info = self.waiting.pop((reply.header.id, addr))
-            reply.header.id = info[4]
-            self.send_reply_to(reply, info[1])
-            if reply.header.rcode == RCODE.NOERROR:
-                # Cache positive result
-                key = "dns:{}:{}".format(info[0].q.qname, info[0].q.qtype)
-                logging.debug("add {} to cache, ttl={}".format(key, info[3]))
-                self.redis.set(key, pickle.dumps(reply), ex=info[3])
-
-
 if __name__ == '__main__':
-    server = Server("config.json")
+    parser = argparse.ArgumentParser(description="A DNS relay")
+    parser.add_argument("-c", "--config", help="config file", required=True)
+    args = parser.parse_args()
+    server = Server(args.config)
     server.serve_forever()
